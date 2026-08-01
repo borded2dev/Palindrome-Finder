0a58efde0f2094848e54ba2c2fe092175b302b30
borded2dev <bored2dev@gmail.com>
2026-07-31 22:16:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 function finder(){
     let text = document.getElementById("txt").value
-    const symbols = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+"
-    ,"[","]","{","}","|",";",":","<",".",">","/","?"," ", "–",",","'","—"]
+    const letters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
             
     let symLess = ""
     let reverse = ""
     let par = ""
-    
-    if (text.length > 1){
-        for (let i = 0; i < text.length; i++){
-            if (!symbols.includes(text[i])){
-                symLess += text[i]
-            }
+
+    function check(){
+      for (let i = 0; i < text.length; i++){
+          if (letters.includes(text[i])){
+            symLess += text[i]
+          }
         }
                 
         for (let i = symLess.length - 1; i >= 0; i--){
-            reverse += symLess[i]
+          reverse += symLess[i]
         }
                 
         if (reverse.toLowerCase() === symLess.toLowerCase()){
-            par += text + " is a palindrome!"
+          par += text + " is a palindrome!"
         } else{
-            par += text + " is NOT a palindrome!"
+          par += text + " is NOT a palindrome!"
         }
+    }
+
+    if (text.length > 1){
+      check()
     } else{
         par += "Sorry, your input is too short or you didn't put one in"
     }
     
-    /*
-    for (let i = 0; i < text.length; i++){
-        if (!symbols.includes(text[i])){
-            symLess += text[i]
-        }
-    }
-            
-    for (let i = symLess.length - 1; i >= 0; i--){
-        reverse += symLess[i]
-    }
-            
-    if (reverse.toLowerCase() === symLess.toLowerCase()){
-        par += text + " is a palindrome!"
-    } else{
-        par += text + " is NOT a palindrome!"
-    }*/
 
     console.log(par)
     document.getElementById("words").innerHTML = par;
 }
+
+
+
+// Listen for key presses anywhere on the page
+document.addEventListener('keydown', function(event) {
+    // Standard property to read the key name
+    //console.log(`Key pressed: ${event.key}`);
+    
+    // Check for a specific key (e.g., the Enter key)
+    if (event.key === 'Enter') {
+        finder()
+    }
+});
